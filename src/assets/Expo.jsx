277187321431dbcf122da7fb8fe04d649becb82f
Leandro Chazarreta <91677149+Chazmann@users.noreply.github.com>
2023-09-17 15:28:31 -0300

    First

---
+++ src/assets/Expo.jsx
@@ -0,0 +1,12 @@
+
+export const Expo = () => {
+    return (
+        <section>
+            <div className='air air1'></div>
+            <div className='air air2'></div>
+            <div className='air air3'></div>
+            <div className='air air4'></div>
+            <div className="expo">dd</div>
+        </section>
+    )
+}
