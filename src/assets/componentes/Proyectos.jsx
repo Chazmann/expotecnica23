b07400a86c2ac6f4d2a7ab1ad73b5d2311363eef
Leandro Chazarreta <91677149+Chazmann@users.noreply.github.com>
2023-10-11 13:52:03 -0300

    updated

--- src/assets/componentes/Proyectos.jsx
+++ src/assets/componentes/Proyectos.jsx
@@ -3,6 +3,7 @@ import { CardContainer } from "./CardContainer"
 export const Proyectos = () => {
   return (
     <div className="filtros">
+        <h1 className="unica">Proyectos que vas a poder ver.</h1>
          <CardContainer modalidad="Informática"/>
          <CardContainer modalidad="Alimentos"/>
 
